495cd398deb6e8c1a1fa91bb3c6c0724685e3ce2
Kyusik Shin <sks156324@gmail.com>
2016-02-23 03:59:24 +0900

    add rat

---
+++ chapter2/rat.scm
@@ -0,0 +1,55 @@
+(load "gcd.scm")
+
+(define (make-rat n d)
+  (let ( (g (gcd n d)) )
+    (cons (/ n g) (/ d g))
+    ) 
+  ) 
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))
+            )
+  )
+ 
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))
+            )
+  )
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))
+            )
+  )
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))
+            )
+  )
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (denom x) (numer y))
+     )
+  )
+
+(define (print-rat rat)
+  (newline)
+  (display (numer rat))
+  (display "/")
+  (display (denom rat))
+  )
+
+(define x (make-rat 1 2))
+(define y (make-rat 2 4))
+
+
+

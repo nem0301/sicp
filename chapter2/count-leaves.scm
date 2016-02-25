389fb4d930d324ce923a67372f1e9d0790162af1
Kyusik Shin <sks156324@gmail.com>
2016-02-25 20:54:10 +0900

    another redefine count-leaves to count-leaves3

--- chapter2/count-leaves.scm
+++ chapter2/count-leaves.scm
@@ -20,11 +20,24 @@
                    (enumerate-tree tree)))
   )
 
+(define (count-leaves3 tree)
+  (accumulate + 
+              0
+              (map (lambda (x) 
+                     (if (pair? x) 
+                         (count-leaves3 x) 
+                         1 )
+                     ) 
+                   tree))
+  )
+
 (define x (cons (list 1 2) (list 3 4)))
 
 (define (test)
   (newline)
-  (display (count-leaves (list x x)))
+  (display (count-leaves (list (list x x) (list x x))))
   (newline)
-  (display (count-leaves2 (list x x)))
+  (display (count-leaves2 (list (list x x) (list x x))))
+  (newline)
+  (display (count-leaves3 (list (list x x) (list x x))))
   )

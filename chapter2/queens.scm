039276a0d13c4a6683c5591348280c96bfd58c95
Kyusik Shin <sks156324@gmail.com>
2016-02-26 06:43:51 +0900

    update pass list

--- chapter2/queens.scm
+++ chapter2/queens.scm
@@ -1,3 +1,38 @@
+(load "map.scm")
+
+(define empty-board nil)
+
+(define (adjoin-position new-row k rest-of-queens)
+  (cons new-row rest-of-queens)
+  )
+
+(define (safe? k position)
+  )
+
+
+
 (define (queens board-size)
+  (define (queen-cols k)
+    (if (= k 0)
+        (list empty-board)
+        (filter (lambda (positions) (safe? k positions))
+                (flatmap (lambda (rest-of-queens)
+                           (map (lambda (new-row)
+                                  (adjoin-position new-row k rest-of-queens)
+                                  )
+                                (enumerate-interval 1 board-size))
+                           )
+                         (queens-cols (- k 1))
+                         )
+                )
+        )
+    )
   
+  (queens-cols board-size)
+  )
+
+(define (test)
+  
+  (testing (list 
+                 ))
   )

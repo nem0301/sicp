755782958474a809e50b144feb031bc27600b96f
Kyusik Shin <sks156324@gmail.com>
2016-03-06 14:42:09 +0900

    solve previous problem and add radian-to-dgree

--- chapter2/complex.scm
+++ chapter2/complex.scm
@@ -131,21 +131,48 @@
   (attach-tag 'polar (cons r a))
   )
 
+(define (radian-to-degree x)
+  (* x 57.2958)
+  )
+
 (define z1 (make-from-real-imag-rectangular 10 20))
 (define z2 (make-from-real-imag-rectangular 20 30))
-(define z3 (make-from-mag-ang-polar 10 20))
-(define z4 (make-from-mag-ang-polar 20 30))
-
+(define z3 (make-from-real-imag-polar 10 20))
+(define z4 (make-from-real-imag-polar 20 30))
 
 (define (test)
-  (testing (list (add-complex z1 z2)
+  (testing (list 
+                 (angle z1)
+                 
+                 "------------"
+                 (add-complex z1 z2)
                  (sub-complex z1 z2)
                  (mul-complex z1 z2)
                  (div-complex z1 z2)
+                 "------------"
                  (add-complex z3 z4)
                  (sub-complex z3 z4)
                  (mul-complex z3 z4)
                  (div-complex z3 z4)
+                 "------------"
+                 (cons (magnitude-rectangular (add-complex z1 z2)) 
+                       (radian-to-degree (angle-rectangular (add-complex z1 z2))))
+                 (cons (magnitude-rectangular (sub-complex z1 z2)) 
+                       (radian-to-degree (angle-rectangular (sub-complex z1 z2))))
+                 (cons (magnitude-rectangular (mul-complex z1 z2)) 
+                       (radian-to-degree (angle-rectangular (mul-complex z1 z2))))
+                 (cons (magnitude-rectangular (div-complex z1 z2)) 
+                       (radian-to-degree (angle-rectangular (div-complex z1 z2))))
+                 "------------"
+                 (cons (magnitude-rectangular (add-complex z3 z4)) 
+                       (radian-to-degree (angle-rectangular (add-complex z3 z4))))
+                 (cons (magnitude-rectangular (sub-complex z3 z4)) 
+                       (radian-to-degree (angle-rectangular (sub-complex z3 z4))))
+                 (cons (magnitude-rectangular (mul-complex z3 z4)) 
+                       (radian-to-degree (angle-rectangular (mul-complex z1 z2))))
+                 (cons (magnitude-rectangular (div-complex z3 z4)) 
+                       (radian-to-degree (angle-rectangular (div-complex z3 z4))))     
+                 
                  )
            )
   )
